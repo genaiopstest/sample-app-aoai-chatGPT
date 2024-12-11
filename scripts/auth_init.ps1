d845087dbb6956432198cb2d852e317eb99ea0fb
Mahi Sundararajan <msunda@microsoft.com>
2024-12-11 06:52:06 -0600

    changes

--- scripts/auth_init.ps1
+++ scripts/auth_init.ps1
@@ -8,4 +8,4 @@ if (Test-Path -Path "/usr") {
 
 Write-Host 'Running "auth_init.py"'
 $appId = $env:AUTH_APP_ID ?? "no-id"
-Start-Process -FilePath $venvPythonPath -ArgumentList "./scripts/auth_init.py --appid $appId" -Wait -NoNewWindow
+Start-Process -FilePath $venvPythonPath -ArgumentList "auth_init.py --appid $appId" -Wait -NoNewWindow
